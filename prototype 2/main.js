0bda3f660d0ee321da79757655fef3fad150aea0
ALAE MAGHCHICH <alaemaghchich2004@gmail.com>
2025-10-15 13:41:55 +0100

    Delete prototype 2 directory

--- prototype 2/main.js
+++
@@ -1,9 +0,0 @@
-const checkboxes = document.querySelectorAll('.list input[type="checkbox"]');
-let counter = document.getElementById("counter");
-function alae(){
-  let completed = document.querySelectorAll('.list input[type="checkbox"]:checked').length;
-  const total = checkboxes.length;
-  counter.textContent = completed + " / " + total + " completed tasks";} //kandiro update llmajmo3
-for (let box of checkboxes){
-  box.onchange = alae;} //kat5dem fax katnbdlo l7ala dyal chek box men check ll unchek wela l3aks
-  //  wm3a tbdel kan3ayeto llfunction alae wkantb9oha
